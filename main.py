2d193884a8737ea91d9d435ef8cd63b44685b696
Anki Vector <84716854+ankivector@users.noreply.github.com>
2022-08-10 22:51:02 +0530

    Update main.py

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ bot = Client(
     'cc-chkbot',
     api_id= 3176510, #get it from https://my.telegram.org/auth
     api_hash="df02110f33f1703026c28801e5fa0731", #get it from https://my.telegram.org/auth
-    bot_token="5192319960:AAFvfj9DVts2QQxN2yWgkSZ3r7dQwfYEo9k", #get it from @Botfather
+    bot_token="1844824479:AAHec9EzAQEmhK0H603BQ9to9kxGzoDWvEw", #get it from @Botfather
     plugins=dict(root="plugins"),
     parse_mode="html")
 
