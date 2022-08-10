ca7c1fe5b53b0c5721c9987d9137490e04e7a9e1
Anki Vector <84716854+ankivector@users.noreply.github.com>
2022-08-10 21:21:36 +0530

    Update buy.py

--- plugins/buy.py
+++ plugins/buy.py
@@ -12,7 +12,7 @@ from pyrogram.types import (
 @Client.on_message(filters.command(['price','buy' ,'purchase', f'buy@{BOT_USERNAME}' , f'price@{BOT_USERNAME}', f'purchase@{BOT_USERNAME}'],prefixes=['.','/','!'],case_sensitive=False) & filters.text)
 async def register(Client,message):
     try: 
-        buttons = [[InlineKeyboardButton('🛒 BUY 🛒', callback_data='buy')]]
+        buttons = [[InlineKeyboardButton('🛒 BUY 🛒', callback_data='mylives')]]
         reply_markup = InlineKeyboardMarkup(buttons)
         text = """CLICK DOWN"""
         await message.reply_text(text=text,reply_to_message_id=message.message_id,reply_markup=reply_markup)
