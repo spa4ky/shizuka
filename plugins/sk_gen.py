b19b83c1d4f5f9c1ad09454e9fe672038a08e833
Anki Vector <84716854+ankivector@users.noreply.github.com>
2022-09-10 22:23:35 +0530

    Create sk_gen.py

---
+++ plugins/sk_gen.py
@@ -0,0 +1,41 @@
+import time
+from pyrogram import Client
+import requests
+from requests.exceptions import ProxyError
+import re
+import bs4
+#from defs import *
+from values import *
+from pyrogram import Client, filters
+import json
+import requests, random, string, time, os
+
+
+sks = []
+def sk_gen(type = 'long', amount = 'x',): 
+    if amount != 'x':
+        amount = int(amount)
+    else:
+        amount = 26
+    genrated = 0
+    while(genrated < amount):
+      genrated += 1
+      if type == "long":
+        skkey = random.choice(['sk_live_51H', 'sk_live_51J'])+''.join(random.choices( string.digits + string.ascii_letters, k = 96))
+      elif type == "short":
+        skkey = "sk_live_"+''.join(random.choices( string.digits + string.ascii_letters, k = 24))
+      else:
+        print("Err")
+      sks.append(skkey + "\n")
+      
+      
+@Client.on_message(filters.command(["skgen", "keygen"], prefixes=[".", "/", "!"], case_sensitive=False) & filters.text)  
+def sk_gen(Client, message):
+  key = message.text.split(None, 1)[1]
+  sk_gen(type="long", amount=key)
+  crd = ''.join(sks)
+  with open("sks.txt", "r+") as s:
+    s.write(crd)
+  sks.clear()   
+  message.reply_document("sks.txt")
+  os.remove("sks.txt")      
