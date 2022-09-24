21b3a15104821f9023053e1591ef608657693685
S P A R K Y <114299352+spa4ky@users.noreply.github.com>
2022-09-25 01:47:57 +0530

    Update Premium.py

--- plugins/Premium.py
+++ plugins/Premium.py
@@ -11,16 +11,17 @@ import json
 
 @Client.on_message(filters.command(["premium"], prefixes=[".", "/", "!"], case_sensitive=False) & filters.text)
 async def sf(Client, message):
-  try:
-        if str(message.from_user.id) + "\n" in admins:
-          iuser = message.from_user.id
-          
-          maindb.update_one({'_id': iuser},{
-            '$set': {
-              "plan": "PAID PLAN",
-              "role": "PAID USER",
-              "status": "P",
-              "credits": credits }}, upsert=False)
-              await message.reply_text("<b>Success..!✅️</b>")
-              else:
-                return await message.reply("Fail..!✖️")
+  credits = message.text.split(None, 1)[1]
+  r_msg = message.reply_to_message
+  iuser = r_msg.from_user.id
+  if message.from_user.id == 1846020026:
+    maindb.update_one({'_id': iuser},{
+      '$set': {
+        "plan": "PAID PLAN",
+        "role": "PAID USER",
+        "status": "P",
+        "credits": credits
+      }}, upsert=False)
+    await message.reply_text("<b>Success..!✅️</b>")
+  else:
+    return await message.reply("Fail..!✖️")
